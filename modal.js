003c712a55383e64cbf189797807581fd9e3e9a8
JonnasWillian <jonnasnogueira2@gmail.com>
2026-02-04 22:33:21 -0300

    atualizando funcoes e curriculo em anexo

--- modal.js
+++ modal.js
@@ -57,15 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // const contactForm = document.querySelector('.contact-form');
-    // if (contactForm) {
-    //     contactForm.addEventListener('submit', function(e) {
-    //         e.preventDefault();
-
-    //         alert('Obrigado pela sua mensagem! Entrarei em contato em breve.');
-    //         this.reset();
-    //     });
-    // }
 });
 
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
